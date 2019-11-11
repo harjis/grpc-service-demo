b3659b3fd46e4f45c71e7bc6a4820c53e93b39dc
Joonas Harjukallio <harjukallio@gmail.com>
2019-11-11 13:27:09 +0200

    Something works

---
+++ src/test/kotlin/com/example/grpcservicedemo/server/services/AnimalServiceTest.kt
@@ -0,0 +1,25 @@
+package com.example.grpcservicedemo.server.services
+
+import com.example.grpcservicedemo.grpc.AnimalOuterClass
+import com.example.grpcservicedemo.grpc.AnimalServiceGrpc
+import io.grpc.ManagedChannelBuilder
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.lognet.springboot.grpc.autoconfigure.GRpcServerProperties
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnimalServiceTest {
+    @Autowired
+    lateinit var gRpcServerProperties: GRpcServerProperties
+
+    @Test
+    fun testSomething() {
+        val channel = ManagedChannelBuilder.forAddress("localhost", gRpcServerProperties.runningPort).usePlaintext().build()
+        val animalStub = AnimalServiceGrpc.newFutureStub(channel)
+        val request = AnimalOuterClass.AnimalRequest.newBuilder().build()
+        val response = animalStub.getAnimals(request).get().animalList
+        Assertions.assertThat(response.size).isEqualTo(2)
+    }
+}
